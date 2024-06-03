e0459bafedc31e022d755ab8d32863e80831dce7
sahilm-ti <sahil.marwaha@trilogy.com>
2024-06-03 13:01:29 +0530

    fix pydantic usage

--- utility.py
+++ utility.py
@@ -7,8 +7,7 @@ from github.PullRequest import PullRequest
 from github.Repository import Repository
 from langchain_core.prompts import ChatPromptTemplate, PromptTemplate
 from langchain_openai import ChatOpenAI
-from pydantic import BaseModel, Field
-from pydantic.v1 import SecretStr
+from langchain_core.pydantic_v1 import BaseModel, Field, SecretStr
 
 
 def get_updated_readme_content(
@@ -17,55 +16,30 @@ def get_updated_readme_content(
     commit_messages: list[str],
     api_key: str,
 ) -> str:
-    prompt = PromptTemplate(
-        template=dedent(
-            """
-                You are a senior software devloper.
-                You are working on a project with a team of developers.
-                Your task is to update the README file of the project, according to the changes made in a pull request.
-                You will be provided with
-                1. A list of changed files in the pull request, including the file name and the changes made.
-                2. The current content of the README file.
-                3. The commit messages associated with the pull request.
-
-                You need to generate the updated README file content based on the provided information.
-                You also need to provide a reason for your changes in the README file.
-                File changes:
-                {diffs}
-                
-                Readme Content:
-                {readme_content}
-                
-                Commit Messages:
-                {commit_messages}
-                """
-        ),
-        input_variables=["diffs", "readme_content", "commit_messages"],
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                dedent(
+                    """
+                        You are a senior software devloper.
+                        You are working on a project with a team of developers.
+                        Your task is to update the README file of the project, according to the changes made in a pull request.
+                        You will be provided with
+                        1. A list of changed files in the pull request, including the file name and the changes made.
+                        2. The current content of the README file.
+                        3. The commit messages associated with the pull request.
+                        You need to generate the updated README file content based on the provided information.
+                        You also need to provide a reason for your changes in the README file.
+                    """
+                ),
+            ),
+            (
+                "human",
+                "File changes:\n{diffs}\n\nReadme Content:\n{readme_content}\n\nCommit Messages:\n{commit_messages}",
+            ),
+        ]
     )
-    # prompt = ChatPromptTemplate.from_messages(
-    #     [
-    #         (
-    #             "system",
-    #             dedent(
-    #                 """
-    #                     You are a senior software devloper.
-    #                     You are working on a project with a team of developers.
-    #                     Your task is to update the README file of the project, according to the changes made in a pull request.
-    #                     You will be provided with
-    #                     1. A list of changed files in the pull request, including the file name and the changes made.
-    #                     2. The current content of the README file.
-    #                     3. The commit messages associated with the pull request.
-    #                     You need to generate the updated README file content based on the provided information.
-    #                     You also need to provide a reason for your changes in the README file.
-    #                 """
-    #             ),
-    #         ),
-    #         (
-    #             "human",
-    #             "File changes:\n{diffs}\n\nReadme Content:\n{readme_content}\n\nCommit Messages:\n{commit_messages}",
-    #         ),
-    #     ]
-    # )
 
     formatted_diffs = "\n".join(
         [f"File: {diff['filename']}\nChanges:\n{diff['patch']}\n" for diff in diffs]
@@ -75,18 +49,21 @@ def get_updated_readme_content(
 
     formatted_commit_messages = "\n".join(commit_messages)
 
-    chat_model = ChatOpenAI(model="gpt-3.5-turbo", api_key=SecretStr(api_key))
+    chat_model = ChatOpenAI(
+        model="gpt-3.5-turbo", api_key=SecretStr(api_key), temperature=0
+    )
     structured_llm = chat_model.with_structured_output(PromptResponse)
 
-    response = structured_llm.invoke(
-        prompt.invoke(
-            {
-                "diffs": formatted_diffs,
-                "readme_content": decoded_readme,
-                "commit_messages": formatted_commit_messages,
-            },
-        )
+    prompt_with_input = prompt.invoke(
+        {
+            "diffs": formatted_diffs,
+            "readme_content": decoded_readme,
+            "commit_messages": formatted_commit_messages,
+        }
     )
+    print(f"Prompt with input: {prompt_with_input}")
+
+    response = structured_llm.invoke(prompt_with_input)
     print(f"Received response: {response}")
     if not isinstance(response, PromptResponse):
         raise ValueError("The response is not a PromptResponse object.")
@@ -122,7 +99,5 @@ def update_readme_and_create_pr(
 
 
 class PromptResponse(BaseModel):
-    updated_readme: str = Field(..., description="The updated README content")
-    reason: str = Field(
-        ..., description="The reason for the changes made in the README"
-    )
+    updated_readme: str = Field(description="The updated README content")
+    reason: str = Field(description="The reason for the changes made in the README")
